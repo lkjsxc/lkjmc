040608dd14e4cdf085cf2640e9c330801107baec
lkjsxc <lkjsxc@gmail.com>
2026-07-06 15:44:18 +0000

    Remove orphaned locale keys and dead common types

--- platforms/jvm/common/src/main/java/com/lkjmc/common/config/LkjmcPluginConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lkjmc.common.config;
-
-public record LkjmcPluginConfig(String daemonUrl, String locale) {
-    public LkjmcPluginConfig {
-        if (locale == null || locale.isBlank()) {
-            locale = "en";
-        }
-    }
-}
